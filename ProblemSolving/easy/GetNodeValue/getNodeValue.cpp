929d6772c4fd317718016d78588dde1d4da3134c
Paul Nema <paul.nema@gmail.com>
2018-11-24 14:26:34 -0800

    initial add

---
+++ ProblemSolving/easy/GetNodeValue/getNodeValue.cpp
@@ -0,0 +1,118 @@
+//  https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+public:
+  int data;
+  SinglyLinkedListNode *next;
+
+  SinglyLinkedListNode(int node_data) {
+    this->data = node_data;
+    this->next = nullptr;
+  }
+};
+
+class SinglyLinkedList {
+public:
+  SinglyLinkedListNode *head;
+  SinglyLinkedListNode *tail;
+
+  SinglyLinkedList() {
+    this->head = nullptr;
+    this->tail = nullptr;
+  }
+
+  void insert_node(int node_data) {
+    SinglyLinkedListNode *node = new SinglyLinkedListNode(node_data);
+
+    if (!this->head) {
+      this->head = node;
+    } else {
+      this->tail->next = node;
+    }
+
+    this->tail = node;
+  }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode *node, string sep,
+                              ofstream &fout) {
+  while (node) {
+    fout << node->data;
+
+    node = node->next;
+
+    if (node) {
+      fout << sep;
+    }
+  }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode *node) {
+  while (node) {
+    SinglyLinkedListNode *temp = node;
+    node = node->next;
+
+    free(temp);
+  }
+}
+
+// Complete the getNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int getNode(SinglyLinkedListNode *head, int &positionFromTail) {
+    if( ! head ) {
+        return( 0 );
+    }
+
+    auto result = getNode( head->next, positionFromTail );
+
+    if( positionFromTail-- == 0 ) {
+        return( head->data );
+    }
+
+    return( result );
+}
+
+int main() {
+  int tests;
+  cin >> tests;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+  for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+    SinglyLinkedList *llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+      int llist_item;
+      cin >> llist_item;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+      llist->insert_node(llist_item);
+    }
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = getNode(llist->head, position);
+
+    cout << result << "\n";
+  }
+
+  return 0;
+}
